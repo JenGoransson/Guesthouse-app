b35a64fe2a3bb279145461617f68c025eb34cc3d
V4N3Z4 <vanessa.zamorano@yh.nackademin.se>
2026-05-17 23:33:46 +0200

    commit

--- src/main/java/se/jennifer/guesthouseapp/guesthouse/room/controller/RoomController.java
+++ src/main/java/se/jennifer/guesthouseapp/guesthouse/room/controller/RoomController.java
@@ -1,12 +1,52 @@
 package se.jennifer.guesthouseapp.guesthouse.room.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import se.jennifer.guesthouseapp.guesthouse.booking.model.Booking;
+import se.jennifer.guesthouseapp.guesthouse.room.model.Room;
 import se.jennifer.guesthouseapp.guesthouse.room.service.RoomService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/room")
 public class RoomController {
+
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public List<Room> getAllRooms(){
+        return roomService.getAllRooms();
+    }
+
+    @GetMapping("/{id}")
+    public Room getRoomById(@PathVariable Long id){
+        return roomService.getRoomById(id);
+    }
+
+    @GetMapping("/{id]/bookings")
+    public List<Booking> getBookingsForRoom(@PathVariable Long id){
+        return roomService.getBookingsForRoom(id);
+    }
+
+    @GetMapping("/available")
+    public List<Room> getAvailableRooms(){
+        return roomService.getAvailableRooms();
+    }
+
+    @PostMapping
+    public Room createRoom(@RequestBody Room room){
+        return roomService.createRoom(room);
+    }
+
+    @PutMapping("/{id}")
+    public Room updateRoom(@PathVariable Long id, @RequestBody Room updateRoom){
+        return  roomService.updateRoom(id, updateRoom);
+    }
+
 
 
 }
