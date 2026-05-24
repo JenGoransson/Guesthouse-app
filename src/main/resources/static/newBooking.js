2bd6231b530d1a911da45a2354201933e77ad766
Jennifer Göransson <jennifer.goransson@yh.nackademin.se>
2026-05-24 11:25:55 +0200

    added customerId in localstorage in app.js so that a booking request gets the customerId correct to backend.

--- src/main/resources/static/newBooking.js
+++ src/main/resources/static/newBooking.js
@@ -19,8 +19,8 @@ document.getElementById("date-form").addEventListener("submit", async function(e
         label.classList.add("room-option");
 
         label.innerHTML = `
-<input type="radio" name="roomId" value="${room.id}" data-type="${room.roomType}">
-Room ${room.roomNumber} – ${room.roomType}
+<input type="radio" name="roomId" value="${room.id}" data-type="${room.type}">
+Room ${room.roomNumber} – ${room.type}
 `;
 
         roomList.appendChild(label);
@@ -34,7 +34,7 @@ document.addEventListener("change", function(e) {
         const roomType = e.target.getAttribute("data-type");
         const extraBedSection = document.getElementById("extra-bed-section");
 
-        if(roomType.toLowerCase()==="double") {
+        if(roomType ==="DOUBLE") {
             extraBedSection.classList.remove("hidden");
         } else {
             extraBedSection.classList.add("hidden");
